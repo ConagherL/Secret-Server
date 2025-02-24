379afec3bfd417dae129f31eb897bb855ce5d1f1
Conagher <23546026+ConagherL@users.noreply.github.com>
2025-02-24 09:28:25 -0500

    Update Secret - Failed Heartbeats by Secret - Total Count Of.sql
    Added lastheartbeat status column

--- Reports/secrets/Secret - Failed Heartbeats by Secret - Total Count Of.sql
+++ Reports/secrets/Secret - Failed Heartbeats by Secret - Total Count Of.sql
@@ -3,6 +3,19 @@ SELECT
     s.LastHeartBeatCheck AS [Last Heartbeat Check],
     ISNULL(f.FolderPath, N'No folder assigned') AS [Folder Path],
     st.SecretTypeName AS [Secret Template],
+    CASE
+        WHEN s.LastHeartBeatStatus = 0 THEN 'Failed'
+        WHEN s.LastHeartBeatStatus = 4 THEN 'UnableToConnect'
+        WHEN s.LastHeartBeatStatus = 5 THEN 'UnknownError'
+        WHEN s.LastHeartBeatStatus = 6 THEN 'IncompatibleHost'
+        WHEN s.LastHeartBeatStatus = 7 THEN 'AccountLockedOut'
+        WHEN s.LastHeartBeatStatus = 8 THEN 'DnsMismatch'
+        WHEN s.LastHeartBeatStatus = 9 THEN 'UnableToValidateServerPublicKey'
+        WHEN s.LastHeartBeatStatus = 10 THEN 'Processing'
+        WHEN s.LastHeartBeatStatus = 11 THEN 'ArgumentError'
+        WHEN s.LastHeartBeatStatus = 12 THEN 'AccessDenied'
+        ELSE 'Unknown'
+    END AS [Last HeartBeat Status],
     (
         SELECT COUNT(*)
         FROM tbSecretLog sl2
@@ -36,4 +49,4 @@ WHERE s.Active = 1
               '1900-01-01'
           )
   )
-ORDER BY s.LastHeartBeatCheck DESC, [Folder Path], [Secret Name];
+ORDER BY s.LastHeartBeatCheck DESC, [Folder Path], [Secret Name]
